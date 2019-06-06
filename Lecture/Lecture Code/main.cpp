6b471d03e6ea6b486fce3c524fa9db46955c2d22
stvn <stteevn0123@gmail.com>
2019-06-06 08:51:10 +0800

    commit

--- Lecture/Lecture Code/main.cpp
+++ Lecture/Lecture Code/main.cpp
@@ -1,30 +1,127 @@
-#include <cstdio>
-using namespace std;
+//====================================================
+//====================================================
+//====================================================
+//Program example 3
 
-enum Color {
-    one = 2,
-    two =4,
-    three,
-    four = 8
+// stacktp.h -- a stack template
+#ifndef STACKTP_H_
+#define STACKTP_H_
+
+template <class Type>
+
+class Stack
+{
+    private:
+    enum {MAX = 10}; // constant specific to class
+    Type items[MAX]; // holds stack items
+    int top; // index for top stack item
+    
+    public:
+    Stack();
+    
+    bool isempty();
+    bool isfull();
+    bool push(const Type & item); // add item to stack
+    bool pop(Type & item); // pop top into item
 };
 
-int f(int a, int b, int &x, int &y) {
-    if(b == 0) {
-        x = 1;
-        y = 0;
-        return a;
-    }
-    int r = f(b, a % b, x, y);
-    int t = x;
-    x = y;
-    y = t - a / b * y;
-    return r;
+template <class Type>
+Stack<Type>::Stack()
+{
+    top = 0;    
 }
 
-int main() {
-    int x, y;
-    int r = f(7, 9, x, y);
-    printf("%d %d %d\n", r, x, y);
-    printf("%d\n", three);
+template <class Type>
+bool Stack<Type>::isempty()
+{
+    return top == 0;
+}
+
+template <class Type>
+bool Stack<Type>::isfull()
+{
+    return top == MAX;
+}
+
+template <class Type>
+bool Stack<Type>::push(const Type & item)
+{
+    if (top < MAX)
+    {
+        items[top++] = item;
+        return true;
+    }
+    else
+        return false;
+}
+
+template <class Type>
+bool Stack<Type>::pop(Type & item)
+{
+    if (top > 0)
+    {
+        item = items[--top];
+        return true;
+    }
+    else
+        return false;
+}
+#endif
+
+//------------------------------------------------------------
+
+// stacktem.cpp -- testing the template stack class
+#include <iostream>
+#include <string>
+#include <cctype>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    Stack<std::string> st; // create an empty stack
+    char ch;
+    std::string po;
+    cout << "Please enter A to add a purchase order,\n"<< "P to process a PO, or Q to quit.\n";
+    
+    while (cin >> ch && std::toupper(ch) != 'Q')
+    {
+        while (cin.get() != '\n')
+            continue;
+        
+        if (!std::isalpha(ch))
+        {
+            cout << '\a';
+            continue;
+        }
+        switch(ch)
+        {
+            case 'A':
+            case 'a': 
+                cout << "Enter a PO number to add: ";
+                cin >> po;
+                if (st.isfull())
+                    cout << "stack already full\n";
+                else
+                    st.push(po);
+                break;
+            case 'P':
+            case 'p': 
+                if (st.isempty())
+                    cout << "stack already empty\n";
+                else 
+                {
+                    st.pop(po);
+                    cout << "PO #" << po << " popped\n";
+                    break;
+                }
+        }
+        
+        cout << "Please enter A to add a purchase order,\n"<< "P to process a PO, or Q to quit.\n";
+    }
+    
+    cout << "Bye\n";
     return 0;
 }
+
+
